cedd71a44f7fe972404f8a6a62558bd574d0757a
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2020-12-11 18:35:36 -0500

    edited files for heroku

--- config/config.js
+++ config/config.js
@@ -15,11 +15,7 @@ module.exports =
     "dialect": "mysql"
   },
   "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "username": "root",
-    "password": process.env.DB_PASSWORD,
-    "database": "companyEvents_db",
-    "host": "127.0.0.1",
+    "use_env_variable": process.env.JAWSDB_URL,
     "dialect": "mysql"
   }
 }
