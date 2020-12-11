95e1047396c4e90dfa22411e970f694eb90d7d27
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2020-12-10 22:03:59 -0500

    Added seeds file so events appear for viewing:

--- schema.sql
+++ schema.sql
@@ -1,12 +1,5 @@
 DROP DATABASE IF EXISTS companyEvents_db;
--- Creates the "passport_demo" database --
+
 CREATE DATABASE companyEvents_db;
 
--- DROP DATABASE IF EXISTS database_test;
--- -- Creates the "login" database --
--- CREATE DATABASE database_test;
-
--- DROP DATABASE IF EXISTS database_production;
--- -- Creates the "login" database --
--- CREATE DATABASE database_production;
 
